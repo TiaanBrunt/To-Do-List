473b28fa830f33514569fef164c64126ff1eaddf
steti101 <steti101@student.otago.ac.nz>
2020-09-11 13:25:58 +1200

    Fully functioning

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -5,6 +5,7 @@ let toDoList = document.querySelector('.to-do-list')
 
 //Listeners
 submitButton.addEventListener('click', addChore);
+toDoList.addEventListener('click', deleteTask);
 
 
 //functions
@@ -13,7 +14,7 @@ function addChore(event){
     event.preventDefault();
     //console.log("test");
     let toDoDiv = document.createElement("div");
-    toDoDiv.classList.add("to-do-list");
+    toDoDiv.classList.add("todo");
 
     let newTask = document.createElement("li");
     newTask.innerText = userInput.value;
@@ -23,16 +24,37 @@ function addChore(event){
     //complete button
 
     let completeButton = document.createElement('button');
-    completeButton.innerText = "Done";
+    completeButton.innerHTML = "<i class='fa fa-check'></i>";
+    completeButton.classList.add("completeBut");
     toDoDiv.appendChild(completeButton);
 
     //remove button
 
     let removeButton = document.createElement('button');
-    removeButton.innerText = "Remove";
+    removeButton.innerHTML = '<i class="fa fa-trash"></i>';
+    removeButton.classList.add("removeBut");
     toDoDiv.appendChild(removeButton); 
 
     toDoList.appendChild(toDoDiv);
 
     userInput.value = "";
+}
+
+function deleteTask(e){
+    let selectedItem = e.target;
+
+    if(selectedItem.classList[0] === 'removeBut'){
+        let todo = selectedItem.parentElement;
+        todo.classList.add("deleteTrans");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        }
+        );
+    }
+
+    if(selectedItem.classList[0] === 'completeBut'){
+        let todo = selectedItem.parentElement;
+        todo.classList.toggle("completed");
+    }
+    
 }
